d63cbdd85618999c7b5f869e7fe8bec1c39f642f
ryan <ryan.mporciuncula@gmail.com>
2024-08-06 17:22:41 -0300

    criacao do arcapouço db

--- src/server.js
+++ src/server.js
@@ -1,31 +1,20 @@
 import http from 'node:http';
-import { emitWarning } from 'node:process';
+import { json } from './middlewares/json.js';
+import { routes } from './routes.js';
 
-const users = []
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer( async(req, res)=>{
     const{method, url} = req
 
-    
+    await json(req, res)
 
-    if (method == 'GET' && url == '/users') {
-        return res
-        .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
+    const route = routes.find(route =>{
+        return route.method == method && route.path == url;
+    })
+
+    if (route){
+        return route.handler(req, res)
     }
-
-
-    if (method == 'POST'&& url == '/users') {
-
-
-        users.push({
-            id:1,
-            name:'zito doido',
-            email:'zitodoido@zito',
-        })
-        return res.writeHead(201).end()
-    }
-
 
     return res.writeHead(404).end()
 
